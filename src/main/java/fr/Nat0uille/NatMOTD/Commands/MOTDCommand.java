6efead58ac7e5f288a9b5f1df7a90005173fd4c1
Natanael <nat0uillepro@gmail.com>
2025-09-13 16:24:28 +0200

    translate config + add selected command

--- src/main/java/fr/Nat0uille/NatMOTD/Commands/MOTDCommand.java
+++ src/main/java/fr/Nat0uille/NatMOTD/Commands/MOTDCommand.java
@@ -19,19 +19,39 @@ public class MOTDCommand implements CommandExecutor {
         MiniMessage mm = MiniMessage.miniMessage();
         Component prefix = mm.deserialize(Main.getConfig().getString("messages.prefix"));
         Component nopermission = mm.deserialize(Main.getConfig().getString("messages.nopermission"));
-        if (!sender.hasPermission("natmotd.reload")) {
-            sender.sendMessage(prefix.append(nopermission));
-            return true;
-        }
+
         if (args.length == 0) {
             sender.sendMessage(prefix.append(mm.deserialize(Main.getConfig().getString("messages.usage"))));
             return true;
         }
         if (args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("natmotd.reload")) {
+                sender.sendMessage(prefix.append(nopermission));
+                return true;
+            }
             Main.reloadConfig();
             Component reloadMessage = mm.deserialize(Main.getConfig().getString("messages.reload"));
             sender.sendMessage(prefix.append(reloadMessage));
             return true;
+        }
+        if (args[0].equalsIgnoreCase("select")) {
+            if (!sender.hasPermission("natmotd.select")) {
+                sender.sendMessage(prefix.append(nopermission));
+                return true;
+            }
+            if (args.length == 1) {
+                sender.sendMessage(prefix.append(mm.deserialize(Main.getConfig().getString("messages.usage-select"))));
+                return true;
+            }
+            try {
+                int selected = Integer.parseInt(args[1]);
+                Main.getConfig().set("motd.selected", selected);
+                Main.saveConfig();
+                Component selectMessage = mm.deserialize(Main.getConfig().getString("messages.select").replace("{number}", args[1]));
+                sender.sendMessage(prefix.append(selectMessage));
+            } catch (NumberFormatException e) {
+                sender.sendMessage(prefix.append(mm.deserialize(Main.getConfig().getString("messages.usage-select"))));
+            }
         }
         return false;
     }
